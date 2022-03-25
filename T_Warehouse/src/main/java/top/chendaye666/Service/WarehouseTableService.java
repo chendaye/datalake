555e2fdcc6637ef4e4c63cef1ff034921e1fb431
chendaye <chendaye666@gmail.com>
2022-03-25 17:06:08 +0800

    save

--- T_Warehouse/src/main/java/top/chendaye666/Service/WarehouseTableService.java
+++ T_Warehouse/src/main/java/top/chendaye666/Service/WarehouseTableService.java
@@ -1,14 +1,19 @@
 package top.chendaye666.Service;
 
 import com.alibaba.fastjson.JSONObject;
+import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import top.chendaye666.Process.WarehouseFlatMap;
 import top.chendaye666.Process.WarehouseProcessFunction;
 import top.chendaye666.dao.NcddDao;
+import top.chendaye666.pojo.CommonTableEntity;
 import top.chendaye666.pojo.NcddLogEntity;
 import top.chendaye666.utils.JsonParamUtils;
+
+import java.util.HashSet;
 
 public class WarehouseTableService {
 
@@ -34,6 +39,10 @@ public class WarehouseTableService {
      * @param jsonParam
      */
     public void transLogToRecord(StreamExecutionEnvironment env, StreamTableEnvironment tEnv, String logTablePath, JsonParamUtils jsonParam){
+        // tables set
+        HashSet<String> tableSet = new HashSet<>();
+        tableSet = jsonParam.getTableSet();
+
         NcddDao ncddDao = new NcddDao();
         Table ncddLog = ncddDao.getNcddLog(env, tEnv, logTablePath);
 
@@ -42,5 +51,6 @@ public class WarehouseTableService {
                 .flatMap(new WarehouseFlatMap(jsonParam.getJson("sourceType")))
                 .process(new WarehouseProcessFunction())
                 .print();
+
     }
 }
--- T_Warehouse/src/main/java/top/chendaye666/Warehouse.java
+++ T_Warehouse/src/main/java/top/chendaye666/Warehouse.java
@@ -1,20 +1,12 @@
 package top.chendaye666;
 
+
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.runtime.state.filesystem.FsStateBackend;
-import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
-import org.apache.flink.table.data.RowData;
-import org.apache.iceberg.flink.TableLoader;
-import org.apache.iceberg.flink.source.FlinkSource;
-import top.chendaye666.Process.WarehouseFlatMap;
-import top.chendaye666.Process.WarehouseProcessFunction;
 import top.chendaye666.Service.WarehouseTableService;
-import top.chendaye666.pojo.NcddLogEntity;
 import top.chendaye666.utils.JsonParamUtils;
-
 import static org.apache.flink.table.api.Expressions.$;
 
 public class Warehouse {
@@ -23,6 +15,7 @@ public class Warehouse {
         ParameterTool params = ParameterTool.fromArgs(args);
         String path = params.get("path", null);
         JsonParamUtils jsonParam = new JsonParamUtils(path);
+
         // flink 运行环境
         System.setProperty("HADOOP_USER_NAME", "hadoop");
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -34,6 +27,8 @@ public class Warehouse {
         // catalog
         WarehouseTableService warehouseTableService = new WarehouseTableService();
         warehouseTableService.createHadoopCatalog(tEnv);
+
+
 
         // 读ncdd_log 表
         // table 转 流
